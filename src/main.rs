25ada710b920527530792dd528fde902de11329e
JamesSnewin <james.snewin@immutable.com>
2023-04-24 08:53:16 +1000

    added .env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Instantiate client with RPC url from env
+    dotenv().ok();
+
+    let provider_url = env::var("PROVIDER_URL").expect("ERROR: provider URL not found");
+
+    println!("Provider URL: {}", provider_url);
+    // Parse CLI arguments
+    // Execute API req based on CLI arguments
+    // Return resp to client
 }
